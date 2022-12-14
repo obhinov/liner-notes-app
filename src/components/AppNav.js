3e2c1de3b440a011ac2ddff93e46f742bf2c0d55
Abhinav Subramani <abhisubramani3@gmail.com>
2022-12-13 19:56:09 -0500

    added components folder

---
+++ src/components/AppNav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginPage from './LoginPage'
+
+export default function AppNav() {
+  return (
+    <div className='AppNav'>
+      <LoginPage />
+    </div>
+  )
+}
---
+++ src/components/creditsPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function creditsPage() {
+  return (
+    <div>creditsPage</div>
+  )
+}
